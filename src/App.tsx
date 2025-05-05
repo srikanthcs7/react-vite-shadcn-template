fce51daafced3a07aa9528d0a829e24c9a4f6b1d
srikanthcs7 <97518385+srikanthcs7@users.noreply.github.com>
2025-05-05 17:41:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { ThemeProvider } from './components/theme-provider';
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <div className="flex flex-col items-center justify-center min-h-screen bg-background text-foreground">
-        <h1 className="text-3xl font-bold mb-4">It is base app, start craftcing stellar apps</h1>
+        <h1 className="text-3xl font-bold mb-4">
+          It is base app! start crafting stellar apps
+        </h1>
+        <h3>Starting point for your next adventure</h3>
       </div>
     </ThemeProvider>
   );
